e7272299ca53e49f9171cd5a62308c6535c0f3b3
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-09-15 16:10:00 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,14 @@
 /**
  * print_most_numbers - prints numbersfrom 0 to 9 except 2 and 4.
  *
- * Return: 0.
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 0 ; n <= 9 ; n++)
 	{
 		if (n != 2 && n != 4)
 			_putchar(n + '0');
