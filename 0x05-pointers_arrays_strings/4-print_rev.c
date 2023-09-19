f6a3dded6bcb6088ce0444aee5004fbfda72361b
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-09-18 17:29:10 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
 
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0, i;
 
-	while (s[index++])
+	for (i = 0 ; s[i] != '\0' ; i++)
 		len++;
 
-	for (index = len - 1 ; index >= 0 ; index--)
-		_putchar(s[index]);
+	for (i = len - 1 ; i >= 0 ; i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
