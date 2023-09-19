196e31d11f2a9cc34d0570f17bd5789ee605bf35
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-09-18 18:11:42 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,11 @@
 void rev_string(char *s)
 {
 	int i;
-	int count;
+	int count = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = count - 1 ; i >= count / 2 ; i--)
+	for (i = 0 ; i < count / 2 ; i++)
 	{
 		char j;
 
