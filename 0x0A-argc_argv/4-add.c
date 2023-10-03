62e27c79a19bd8978e27d03ff619a66289f1525c
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-10-02 17:06:43 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1 ; a < argc ; a++)
+	{
+		for (b = 0 ; argv[a][b]; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
