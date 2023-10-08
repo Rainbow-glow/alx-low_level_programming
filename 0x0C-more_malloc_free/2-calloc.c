156544a903cbf7580b5388f5b5501fc83ce7ce7c
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-10-08 09:56:47 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array using malloc.
+ * @nmemb: the number of the members of the array.
+ * @size: the size of the array.
+ *
+ * Return: a pointer to the newly allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *allo;
+	unsigned int i;
+
+	if (nmemb == 0 && size == 0)
+		return (NULL);
+
+	mem = malloc(nmemb * size);
+
+	if (mem == NULL)
+		return (NULL);
+
+	allo = mem;
+
+	for (i = 0 ; i < (nmemb * size); i++)
+		allo[i] = '\0';
+
+	return (mem);
+}
