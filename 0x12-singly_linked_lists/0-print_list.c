85a4b1195e95f9d8da017934868007920bb50448
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-10-19 14:19:23 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,15 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
+
 		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }
