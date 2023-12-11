052c6a46c9b7703022a56cbeab2b1e6117c3969f
Rainbow-glow <emmanuellanweze97@gmail.com>
2023-12-11 13:58:56 -0800

    more

---
+++ 0x18-dynamic_libraries/100-operation.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+/**
+ * add - adds integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: the results.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substracts integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: the results.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - divides integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: the results.
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - multipies integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: the results.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - gets the modules of integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: the results.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
